aaf98f9ef92ffd1ddd94e1a2b8d90f1fb8ab2f0e
木木木ding <823715808@qq.com>
2020-07-22 23:28:36 +0800

    4 tree lag

--- CodingMeansOffer/src/tree/MirrorTree.java
+++ CodingMeansOffer/src/tree/MirrorTree.java
@@ -1,4 +1,39 @@
 package tree;
 
+import utils.TreeNode;
+
+import java.util.Stack;
+
 public class MirrorTree {
+
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left, right = root.right;
+        root.left = mirrorTree(right);
+        root.right = mirrorTree(left);
+        return root;
+    }
+
+    public TreeNode mirrorTreeByStack(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+        }
+        return root;
+    }
 }
--- Leetcode/src/tree/InvertBinaryTree.java
+++ Leetcode/src/tree/InvertBinaryTree.java
@@ -1,4 +1,15 @@
 package tree;
 
+import utils.TreeNode;
+
 public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode leftCopy = root.left, rightCopy = root.right;
+        root.left = invertTree(rightCopy);
+        root.right = invertTree(leftCopy);
+        return root;
+    }
 }
